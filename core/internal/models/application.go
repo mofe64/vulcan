75b21048cd2e1188d29ef2b0dc90e49b65d2a2d3
mofe64 <ogunbiyioladapo33@gmail.com>
2025-04-01 15:49:58 +0100

    refactored stuff

--- core/internal/models/application.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Application struct {
-	Id             int64  `json:"id"`
-	Name           string `json:"name"`
-	OwnerId        string `json:"ownerId"`
-	RepositoryType string `json:"repoType"`
-}

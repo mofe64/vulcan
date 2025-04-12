e85e0500d1067ecb6af189984b9db27e521acd8e
mofe64 <ogunbiyioladapo33@gmail.com>
2025-04-12 11:06:39 +0100

    application model

--- core/pkg/models/application.go
+++ core/pkg/models/application.go
@@ -1,9 +1,16 @@
 package models
 
 type Application struct {
-	Id             int64  `json:"id"`
-	Name           string `json:"name"`
-	OwnerId        string `json:"ownerId"`
-	RepositoryType string `json:"repoType"`
-	SourceCodeLink Link   `json:"sourceCodeLink"`
+	Id             int64          `json:"id"`
+	OwnerId        int64          `json:"ownerId"`
+	Name           string         `json:"name"`
+	SourceCodeLink SourceCodeLink `json:"sourceCodeLink"`
+}
+
+type SourceCodeLink struct {
+	RepoUrl           string `json:"repoUrl"`
+	RepoName          string `json:"repoName"`
+	RepoType          string `json:"repoType"`
+	RepoBranchName    string `json:"repoBranchName"`
+	WebhookIdentifier string `json:"webhookIdentifier"`
 }

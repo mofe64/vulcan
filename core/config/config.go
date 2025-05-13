5bddf9e86675b702b3b84f0435b60937be97f83f
mofe64 <ogunbiyioladapo33@gmail.com>
2025-05-13 07:25:52 +0100

    restarted project

--- core/config/config.go
+++
@@ -1,6 +0,0 @@
-package config
-
-type VulkanConfig struct {
-	DBType       string `json:"dbType"` // "sqlite", "postgres", "mysql"
-	DBConnection string `json:"dbConnection"`
-}
--- core/pkg/errors/sentinel.go
+++
@@ -1,6 +0,0 @@
-package vulkan_errors
-
-import "errors"
-
-var ErrGitNotInstalled = errors.New("git not installed")
-var ErrGitCloneFailed = errors.New("git clone failed")

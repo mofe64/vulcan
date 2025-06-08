24e4870454b00a06330de3331b2f3d4dcfa30c7f
mofe64 <ogunbiyioladapo33@gmail.com>
2025-06-08 12:51:33 +0100

    auth oidc structs

---
+++ internal/auth/auth.go
@@ -0,0 +1,39 @@
+package auth
+
+import (
+	"context"
+
+	"github.com/coreos/go-oidc/v3/oidc"
+	"github.com/mofe64/vulkan/internal/config"
+	"golang.org/x/oauth2"
+)
+
+type VulkanAuth struct {
+	Provider  *oidc.Provider
+	OAuth2Cfg *oauth2.Config
+	Verifier  *oidc.IDTokenVerifier
+}
+
+func BuildVulkanAuth(ctx context.Context, cfg *config.VulkanConfig) (*VulkanAuth, error) {
+	// "https://dex.vulkan.strawhatengineer.com/dex"
+	provider, err := oidc.NewProvider(ctx, cfg.DEX_URL)
+	if err != nil {
+		return nil, err
+	}
+	oAuth2Cfg := &oauth2.Config{
+		ClientID:     cfg.OIDC_CLIENT_ID,
+		ClientSecret: cfg.OIDC_CLIENT_SECRET,
+		Endpoint:     provider.Endpoint(), // pulls /auth & /token URLs from Dex discovery
+		RedirectURL:  "https://api.vulkan.strawhatengineer.com/api/auth/callback",
+		Scopes:       []string{oidc.ScopeOpenID, "email", "profile", "offline_access"},
+	}
+
+	verifier := provider.Verifier(&oidc.Config{
+		ClientID: cfg.OIDC_CLIENT_ID,
+	})
+	return &VulkanAuth{
+		Provider:  provider,
+		OAuth2Cfg: oAuth2Cfg,
+		Verifier:  verifier,
+	}, nil
+}

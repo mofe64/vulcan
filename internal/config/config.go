e5539a402ca2bdee69da0cba6446e41b962de3a7
mofe64 <ogunbiyioladapo33@gmail.com>
2025-06-08 12:51:53 +0100

    server config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,13 +1,26 @@
 package config
 
-import "github.com/caarlos0/env"
+import (
+	"time"
 
+	"github.com/caarlos0/env"
+)
+
+// OPA_URL has been set in the environment variable OPA_URL
+// this env var is loaded into container by the helm deployment.yaml file for the api server
 type VulkanConfig struct {
-	DBURL            string `env:"VULKAN_DATABASE_URL,required"`
-	OIDCJWKSURL      string `env:"OIDC_JWKS_URL,required"`
-	VulkanServerPort string `env:"VULKAN_PORT"           default:"9021"`
-	InCluster        bool   `env:"K8S_IN_CLUSTER" default:"false"`
-	LOG_LEVEL        string `env:"LOG_LEVEL" default:"info"`
+	DBURL              string        `env:"VULKAN_DATABASE_URL,required"`
+	OIDCJWKSURL        string        `env:"OIDC_JWKS_URL,required"`
+	VulkanServerPort   string        `env:"VULKAN_PORT"           default:"9021"`
+	InCluster          bool          `env:"K8S_IN_CLUSTER" default:"false"`
+	LOG_LEVEL          string        `env:"LOG_LEVEL" default:"info"`
+	OpaUrl             string        `env:"OPA_URL"`
+	OpaPolicy_Path     string        `env:"OPA_POLICY_PATH"`
+	OpaReqTIMEOUT      time.Duration `env:"OPA_REQ_TIMEOUT" default:"300"`
+	NATS_URL           string        `env:"NATS_URL"`
+	DEX_URL            string        `env:"DEX_URL,required"`
+	OIDC_CLIENT_ID     string        `env:"VULKAN_OIDC_CLIENT_ID,required"`
+	OIDC_CLIENT_SECRET string        `env:"VULKAN_OIDC_CLIENT_SECRET,required"`
 }
 
 // Load parses the environment into a Config struct and returns it.

08a02b30fefb24d1db638469de2438f2a1a9da86
mofe64 <ogunbiyioladapo33@gmail.com>
2025-06-08 12:52:52 +0100

    org dto

--- internal/dto/org.go
+++ internal/dto/org.go
@@ -3,13 +3,15 @@ package dto
 import "time"
 
 type Org struct {
-	Id        string    `json:"id"`
-	Name      string    `json:"name"`
-	OwnerId   string    `json:"owner_id"`
-	CreatedAt time.Time `json:"created_at"`
+	Id         string    `json:"id"`
+	Name       string    `json:"name"`
+	OwnerId    string    `json:"owner_id"`
+	OwnerEmail string    `json:"owner_email"`
+	CreatedAt  time.Time `json:"created_at"`
 }
 
 type CreateOrgRequest struct {
-	Name    string `json:"name"`
-	OwnerId string `json:"owner_id"`
+	Name       string `json:"name"`
+	OwnerId    string `json:"owner_id"`
+	OwnerEmail string `json:"owner_email"`
 }

750d9f97683bcdfb7b338b51b73ee91e572a9b1d
mofe64 <ogunbiyioladapo33@gmail.com>
2025-06-19 16:43:21 +0100

    cluster condtions

---
+++ operator/api/v1alpha1/conditions.go
@@ -0,0 +1,9 @@
+package v1alpha1
+
+const (
+	Ready        string = "Ready"
+	Provisioning string = "Provisioning"
+	Degraded     string = "Degraded"
+	Deleting     string = "Deleting"
+	Error        string = "Error"
+)

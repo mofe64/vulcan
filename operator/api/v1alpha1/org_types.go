eb8a4970b58e6e8d98f552a654e788930684a169
mofe64 <ogunbiyioladapo33@gmail.com>
2025-06-20 17:43:44 +0100

    updated ord crd

--- operator/api/v1alpha1/org_types.go
+++ operator/api/v1alpha1/org_types.go
@@ -1,40 +1,22 @@
-/*
-Copyright 2025.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // OrgSpec defines the desired state of Org.
 type OrgSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
 	// DisplayName is a human-readable name for the organization
+	// should be unique within the cluster
+	// +kubebuilder:validation:Unique
 	// +kubebuilder:validation:MinLength=3
 	// +kubebuilder:validation:MaxLength=100
+	// +kubebuilder:validation:Required
 	DisplayName string `json:"displayName"`
 
 	// OwnerEmail receieves system notifications and is the primary contact for the organization
 	// +kubebuilder:validation:Format=email
+	// +kubebuilder:validation:Required
 	OwnerEmail string `json:"ownerEmail"`
 
 	// Quota defines the resource limits for the organization will be enforced by org controller
@@ -52,18 +34,24 @@ type OrgQuota struct {
 
 // OrgStatus defines the observed state of Org.
 type OrgStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Conditions represent the latest available observations
+	// of the resource’s state.
+	// +patchMergeKey=type
+	// +patchStrategy=merge
+	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
 
-	Phase   string      `json:"phase,omitempty"`   // Phase indicates the current lifecycle phase of the organization (e.g., Pending, Ready, Failed)
-	Metrics OrgCounters `json:"metrics,omitempty"` // Metrics contains counters for the number of clusters and apps in the organization
+	Metrics OrgCounters `json:"metrics,omitempty"` // Metrics contains counters for the number of clusters, projects and apps in the organization
 }
 
-// OrgCounters holds counters for the number of clusters and applications in the organization.
+// OrgCounters holds counters for the number of clusters , projectsand applications in the organization.
 // It is used to track the live usage of resources within the organization.
 type OrgCounters struct {
 	// Clusters is the number of clusters created in this organization
 	Clusters int32 `json:"clusters,omitempty"`
+
+	// Projects is the number of projects that can be created in this organization
+	Projects int32 `json:"projects,omitempty"`
+
 	// Apps is the number of applications created in this organization
 	Apps int32 `json:"apps,omitempty"`
 }

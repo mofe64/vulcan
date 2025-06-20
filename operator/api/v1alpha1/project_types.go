7a5a650b036cc41f059f5fb4208d9f94f49c5152
mofe64 <ogunbiyioladapo33@gmail.com>
2025-06-20 17:43:54 +0100

    updated project crd

--- operator/api/v1alpha1/project_types.go
+++ operator/api/v1alpha1/project_types.go
@@ -1,34 +1,12 @@
-/*
-Copyright 2025.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // ProjectSpec defines the desired state of Project.
 type ProjectSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// OrgRef is the reference to the organization this project belongs to
+	// OrgRef is the reference to the name of theorganization this project belongs to
 	// +kubebuilder:validation:Pattern=`^[0-9a-fA-F-]{36}$`
 	OrgRef string `json:"orgRef"`
 
